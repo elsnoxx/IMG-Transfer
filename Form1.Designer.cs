b198057c9aeaa6a3bc5fd9e414f7b0254e9333fd
Richard Ficek <richard.ficek@hyundai-autoever.cz>
2024-07-01 13:32:10 +0200

    init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace IMGTransefere
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            progressBar = new ProgressBar();
+            Path = new TextBox();
+            imgTransfer = new Button();
+            logo = new PictureBox();
+            checkBox1 = new CheckBox();
+            comboBox1 = new ComboBox();
+            Choose = new Button();
+            FolderPath = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            comboBox2 = new ComboBox();
+            label5 = new Label();
+            ((System.ComponentModel.ISupportInitialize)logo).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(360, 106);
+            button1.Name = "button1";
+            button1.Size = new Size(87, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Otevřít";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 23);
+            label1.TabIndex = 0;
+            // 
+            // progressBar
+            // 
+            progressBar.Location = new Point(37, 317);
+            progressBar.Name = "progressBar";
+            progressBar.Size = new Size(410, 23);
+            progressBar.TabIndex = 1;
+            // 
+            // Path
+            // 
+            Path.Location = new Point(37, 106);
+            Path.Name = "Path";
+            Path.Size = new Size(317, 23);
+            Path.TabIndex = 2;
+            // 
+            // imgTransfer
+            // 
+            imgTransfer.BackColor = Color.PeachPuff;
+            imgTransfer.ForeColor = SystemColors.ControlText;
+            imgTransfer.Location = new Point(172, 254);
+            imgTransfer.Name = "imgTransfer";
+            imgTransfer.Size = new Size(131, 57);
+            imgTransfer.TabIndex = 3;
+            imgTransfer.Text = "Převést";
+            imgTransfer.UseVisualStyleBackColor = false;
+            imgTransfer.Click += imgTransfer_Click;
+            // 
+            // logo
+            // 
+            logo.Location = new Point(12, 12);
+            logo.Name = "logo";
+            logo.Size = new Size(200, 71);
+            logo.TabIndex = 4;
+            logo.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(37, 184);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(266, 19);
+            checkBox1.TabIndex = 5;
+            checkBox1.Text = "Uložení na stejné místo jako nahraný obrázek.";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(106, 135);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(50, 23);
+            comboBox1.TabIndex = 6;
+            // 
+            // Choose
+            // 
+            Choose.Location = new Point(358, 209);
+            Choose.Name = "Choose";
+            Choose.Size = new Size(89, 23);
+            Choose.TabIndex = 7;
+            Choose.Text = "Vybrat složku";
+            Choose.UseVisualStyleBackColor = true;
+            Choose.Click += Choose_Click;
+            // 
+            // FolderPath
+            // 
+            FolderPath.Location = new Point(37, 209);
+            FolderPath.Name = "FolderPath";
+            FolderPath.Size = new Size(317, 23);
+            FolderPath.TabIndex = 8;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial Black", 18F, FontStyle.Bold);
+            label2.Location = new Point(218, 28);
+            label2.Name = "label2";
+            label2.Size = new Size(256, 33);
+            label2.TabIndex = 9;
+            label2.Text = "Převodník obrázků";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F);
+            label3.Location = new Point(40, 135);
+            label3.Name = "label3";
+            label3.Size = new Size(60, 21);
+            label3.TabIndex = 10;
+            label3.Text = "Formát";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F);
+            label4.Location = new Point(218, 137);
+            label4.Name = "label4";
+            label4.Size = new Size(115, 21);
+            label4.TabIndex = 11;
+            label4.Text = "Míra komprese";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(339, 135);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(55, 23);
+            comboBox2.TabIndex = 12;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F);
+            label5.Location = new Point(400, 137);
+            label5.Name = "label5";
+            label5.Size = new Size(23, 21);
+            label5.TabIndex = 13;
+            label5.Text = "%";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(484, 361);
+            Controls.Add(label5);
+            Controls.Add(comboBox2);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(FolderPath);
+            Controls.Add(Choose);
+            Controls.Add(comboBox1);
+            Controls.Add(checkBox1);
+            Controls.Add(logo);
+            Controls.Add(imgTransfer);
+            Controls.Add(Path);
+            Controls.Add(progressBar);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            MaximizeBox = false;
+            MaximumSize = new Size(500, 400);
+            MinimumSize = new Size(500, 400);
+            Name = "Form1";
+            Text = "Převodník obrázků";
+            ((System.ComponentModel.ISupportInitialize)logo).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private ProgressBar progressBar;
+        private TextBox Path;
+        private Button imgTransfer;
+        private PictureBox logo;
+        private CheckBox checkBox1;
+        private ComboBox comboBox1;
+        private Button Choose;
+        private TextBox FolderPath;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private ComboBox comboBox2;
+        private Label label5;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace IMGTransefere
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Nastavení vlastností vyberu checkBox1
+            checkBox1.Checked = true;
+
+            // Nastavení vlastností vyberu comboBox1
+            comboBox1.Items.Add("JPG");
+            comboBox1.Items.Add("PNG");
+            comboBox1.Items.Add("JPEG");
+            comboBox1.SelectedIndex = 0;
+
+            // Nastavení vlastností vyberu comboBox2
+            comboBox2.Items.Add("100");
+            comboBox2.Items.Add("80");
+            comboBox2.Items.Add("75");
+            comboBox2.Items.Add("50");
+            comboBox2.Items.Add("25");
+            comboBox2.SelectedIndex = 0;
+
+            // Nastavení vlastností ProgressBar
+            progressBar.Minimum = 0;
+            progressBar.Maximum = 100;
+            progressBar.Value = 0;
+            progressBar.Visible = true;
+
+
+            FolderPath.Visible = false;
+            Choose.Visible = false;
+
+            // Příklad načtení a nastavení obrázku z disku
+            logo.Image = Image.FromFile("logoSmall1.png");
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Všechny soubory (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Zde můžete pracovat se vybraným souborem
+                    string selectedFileName = openFileDialog.FileName;
+                    // Například vypište cestu k souboru do konzole
+
+                    Path.Text = selectedFileName;
+                }
+            }
+        }
+
+        private void imgTransfer_Click(object sender, EventArgs e)
+        {
+            if (System.IO.Path.GetExtension(Path.Text).Equals("." + comboBox1.Text, StringComparison.OrdinalIgnoreCase))
+            {
+                MessageBox.Show("Vybrali jste stejný formát jako je již v obrázku. Vyberte prosím jiný formát.");
+                return;
+            }
+
+            if (Path.Text == "")
+            {
+                MessageBox.Show("Vyberte soubor k převodu.");
+                return;
+            }
+            if (FolderPath.Text == "" && !checkBox1.Checked)
+            {
+                MessageBox.Show("Vyberte složku kam chcete soubor uložit.");
+                return;
+            }
+
+            progressBar.Visible = true;
+            string vybranaPolozka = comboBox1.SelectedItem.ToString();
+
+            progressBar.Value += 10;
+
+            // Načtení obrázku
+            Bitmap originalImage = new Bitmap(Path.Text);
+            progressBar.Value += 10;
+
+            // Komprimace obrázku
+            EncoderParameters encoderParameters = new EncoderParameters(1);
+            encoderParameters.Param[0] = new EncoderParameter(Encoder.Quality, long.Parse(comboBox2.Text));
+            progressBar.Value += 10;
+
+            ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
+            MemoryStream memoryStream = new MemoryStream();
+
+            originalImage.Save(memoryStream, jpgEncoder, encoderParameters);
+            progressBar.Value += 10;
+
+            string compressedImagePath = "";
+            // Nový název pro komprimovaný obrázek
+            if (checkBox1.Checked)
+            {
+                compressedImagePath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Path.Text) + "\\" + System.IO.Path.GetFileNameWithoutExtension(Path.Text) + "." + comboBox1.Text.ToLower());
+                progressBar.Value += 10;
+            }
+            else
+            {
+                compressedImagePath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(FolderPath.Text) + "\\" + System.IO.Path.GetFileNameWithoutExtension(Path.Text) + "." + comboBox1.Text.ToLower());
+                progressBar.Value += 10;
+            }
+
+
+
+
+            // Uložení komprimovaného obrázku do výstupní složky
+            File.WriteAllBytes(compressedImagePath, memoryStream.ToArray());
+            for (int i = 0; i <= 40; i++)
+            {
+                progressBar.Value++;
+            }
+
+
+            progressBar.Visible = false;
+            progressBar.Value = 0;
+        }
+
+        private ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Choose.Visible == true)
+            {
+                FolderPath.Visible = false;
+                Choose.Visible = false;
+            }
+            else
+            {
+                FolderPath.Visible = true;
+                Choose.Visible = true;
+            }
+        }
+
+        private void Choose_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.Description = "Vyberte složku";
+                folderBrowserDialog.RootFolder = Environment.SpecialFolder.MyComputer;
+
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Získání vybrané složky
+                    string selectedFolderPath = folderBrowserDialog.SelectedPath;
+
+                    // Nastavení cesty složky do FolderPath.Text
+                    FolderPath.Text = selectedFolderPath;
+                }
+            }
+        }
+    }
+}
