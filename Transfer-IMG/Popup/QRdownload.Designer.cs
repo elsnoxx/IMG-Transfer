1ef71ec8d5620cfe524eb09779b6cf82a5661871
Richard Ficek <richard.ficek@hyundai-autoever.cz>
2024-08-09 12:10:13 +0200

    QR codes done

--- Transfer-IMG/Popup/QRdownload.Designer.cs
+++ Transfer-IMG/Popup/QRdownload.Designer.cs
@@ -28,10 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QRdownload));
             this.qrCodePictureBox = new System.Windows.Forms.PictureBox();
             this.saveLabel = new System.Windows.Forms.Label();
             this.btnSaveQRSVG = new System.Windows.Forms.Button();
             this.btnSaveQR = new System.Windows.Forms.Button();
+            this.widthBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.savAsPDF = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.qrCodePictureBox)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,7 +43,7 @@
             // 
             this.qrCodePictureBox.BackColor = System.Drawing.Color.White;
             this.qrCodePictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.qrCodePictureBox.Location = new System.Drawing.Point(40, 99);
+            this.qrCodePictureBox.Location = new System.Drawing.Point(12, 12);
             this.qrCodePictureBox.Name = "qrCodePictureBox";
             this.qrCodePictureBox.Size = new System.Drawing.Size(250, 250);
             this.qrCodePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -51,7 +55,7 @@
             this.saveLabel.AutoSize = true;
             this.saveLabel.Font = new System.Drawing.Font("Showcard Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.saveLabel.ForeColor = System.Drawing.Color.ForestGreen;
-            this.saveLabel.Location = new System.Drawing.Point(486, 352);
+            this.saveLabel.Location = new System.Drawing.Point(336, 12);
             this.saveLabel.Name = "saveLabel";
             this.saveLabel.Size = new System.Drawing.Size(113, 27);
             this.saveLabel.TabIndex = 15;
@@ -60,34 +64,88 @@
             // 
             // btnSaveQRSVG
             // 
-            this.btnSaveQRSVG.Location = new System.Drawing.Point(582, 299);
+            this.btnSaveQRSVG.FlatAppearance.BorderSize = 0;
+            this.btnSaveQRSVG.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSaveQRSVG.Font = new System.Drawing.Font("Trebuchet MS", 20.25F, System.Drawing.FontStyle.Bold);
+            this.btnSaveQRSVG.ForeColor = System.Drawing.Color.White;
+            this.btnSaveQRSVG.Image = ((System.Drawing.Image)(resources.GetObject("btnSaveQRSVG.Image")));
+            this.btnSaveQRSVG.Location = new System.Drawing.Point(398, 212);
             this.btnSaveQRSVG.Name = "btnSaveQRSVG";
             this.btnSaveQRSVG.Size = new System.Drawing.Size(100, 50);
             this.btnSaveQRSVG.TabIndex = 14;
-            this.btnSaveQRSVG.Text = "Uložit jako SVG";
+            this.btnSaveQRSVG.Text = "SVG";
             this.btnSaveQRSVG.UseVisualStyleBackColor = true;
             this.btnSaveQRSVG.Click += new System.EventHandler(this.btnSaveQRSVG_Click);
             // 
             // btnSaveQR
             // 
-            this.btnSaveQR.Location = new System.Drawing.Point(405, 299);
+            this.btnSaveQR.FlatAppearance.BorderSize = 0;
+            this.btnSaveQR.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSaveQR.Font = new System.Drawing.Font("Trebuchet MS", 20.25F, System.Drawing.FontStyle.Bold);
+            this.btnSaveQR.ForeColor = System.Drawing.Color.White;
+            this.btnSaveQR.Image = ((System.Drawing.Image)(resources.GetObject("btnSaveQR.Image")));
+            this.btnSaveQR.Location = new System.Drawing.Point(292, 212);
             this.btnSaveQR.Name = "btnSaveQR";
             this.btnSaveQR.Size = new System.Drawing.Size(100, 50);
             this.btnSaveQR.TabIndex = 13;
-            this.btnSaveQR.Text = "Uložit jako PNG";
+            this.btnSaveQR.Text = "PNG";
             this.btnSaveQR.UseVisualStyleBackColor = true;
             this.btnSaveQR.Click += new System.EventHandler(this.btnSaveQR_Click);
+            // 
+            // widthBox
+            // 
+            this.widthBox.FormattingEnabled = true;
+            this.widthBox.Items.AddRange(new object[] {
+            "1000",
+            "800",
+            "750",
+            "500",
+            "250"});
+            this.widthBox.Location = new System.Drawing.Point(360, 74);
+            this.widthBox.Name = "widthBox";
+            this.widthBox.Size = new System.Drawing.Size(64, 21);
+            this.widthBox.TabIndex = 26;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(292, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Šířka";
+            // 
+            // savAsPDF
+            // 
+            this.savAsPDF.FlatAppearance.BorderSize = 0;
+            this.savAsPDF.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.savAsPDF.Font = new System.Drawing.Font("Trebuchet MS", 20.25F, System.Drawing.FontStyle.Bold);
+            this.savAsPDF.ForeColor = System.Drawing.Color.White;
+            this.savAsPDF.Image = ((System.Drawing.Image)(resources.GetObject("savAsPDF.Image")));
+            this.savAsPDF.Location = new System.Drawing.Point(292, 156);
+            this.savAsPDF.Name = "savAsPDF";
+            this.savAsPDF.Size = new System.Drawing.Size(100, 50);
+            this.savAsPDF.TabIndex = 30;
+            this.savAsPDF.Text = "PDF";
+            this.savAsPDF.UseVisualStyleBackColor = true;
+            this.savAsPDF.Click += new System.EventHandler(this.savAsPDF_Click);
             // 
             // QRdownload
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(195)))), ((int)(((byte)(207)))));
+            this.ClientSize = new System.Drawing.Size(507, 274);
+            this.Controls.Add(this.savAsPDF);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.widthBox);
             this.Controls.Add(this.saveLabel);
             this.Controls.Add(this.btnSaveQRSVG);
             this.Controls.Add(this.btnSaveQR);
             this.Controls.Add(this.qrCodePictureBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "QRdownload";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "QRdownload";
             ((System.ComponentModel.ISupportInitialize)(this.qrCodePictureBox)).EndInit();
             this.ResumeLayout(false);
@@ -101,5 +159,8 @@
         private System.Windows.Forms.Label saveLabel;
         private System.Windows.Forms.Button btnSaveQRSVG;
         private System.Windows.Forms.Button btnSaveQR;
+        private System.Windows.Forms.ComboBox widthBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button savAsPDF;
     }
 }
--- Transfer-IMG/Popup/QRdownload.cs
+++ Transfer-IMG/Popup/QRdownload.cs
@@ -1,13 +1,11 @@
 ﻿using QRCoder;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System.IO;
+using System.Drawing.Drawing2D;
 
 namespace Transfer_IMG.Popup
 {
@@ -20,12 +18,14 @@ namespace Transfer_IMG.Popup
 
             qrCodePictureBox.Image = qrCodeImage;
             QRtext_SVG = QRtext;
+            widthBox.SelectedIndex = 0;
         }
 
         private void btnSaveQR_Click(object sender, EventArgs e)
         {
-            // Bitmapa pro zobrazení QR kódu
-            Bitmap qrCodeImage = (Bitmap)qrCodePictureBox.Image;
+
+            // Změna velikosti obrázku podle zadaných rozměrů
+            System.Drawing.Image resizedQrCodeImage = ResizeImage(qrCodePictureBox.Image, widthBox);
 
             // Uložení QR kódu do souboru
             using (SaveFileDialog saveFileDialog = new SaveFileDialog())
@@ -38,7 +38,7 @@ namespace Transfer_IMG.Popup
                 {
                     if (!string.IsNullOrEmpty(saveFileDialog.FileName))
                     {
-                        qrCodeImage.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                        resizedQrCodeImage.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Png);
                         saveLabel_dysplayed();
                     }
                 }
@@ -50,18 +50,95 @@ namespace Transfer_IMG.Popup
             saveLabel.Visible = true;
         }
 
+        private static System.Drawing.Image ResizeImage(System.Drawing.Image imgToResize, ComboBox widthBox)
+        {
+            int Width;
+
+            // Kontrola, zda je vstup validní, pokud ne, zobrazí se chybová zpráva a metoda se ukončí.
+            if (!int.TryParse(widthBox.Text, out Width))
+            {
+                MessageBox.Show("Zadal jsi špatnou velikost");
+                return null; // Ukončí metodu a vrátí null
+            }
+
+            Size size = new Size(Width, Width);
+            // Get the image current width
+            int sourceWidth = imgToResize.Width;
+            // Get the image current height
+            int sourceHeight = imgToResize.Height;
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+            // Calculate width and height with new desired size
+            nPercentW = ((float)size.Width / (float)sourceWidth);
+            nPercentH = ((float)size.Height / (float)sourceHeight);
+            nPercent = Math.Min(nPercentW, nPercentH);
+            // New Width and Height
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+            Bitmap b = new Bitmap(destWidth, destHeight);
+            Graphics g = Graphics.FromImage((System.Drawing.Image)b);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            // Draw image with new width and height
+            g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+            g.Dispose();
+            return (System.Drawing.Image)b;
+
+        }
+
+        private Bitmap GenerateQrCodeBitmap(string qrText, int size)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrText, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+
+            // Vytvoření bitmapy QR kódu s požadovanou velikostí
+            Bitmap qrCodeBitmap = qrCode.GetGraphic(20, Color.Black, Color.White, true);
+            return qrCodeBitmap;
+        }
+
+        private Bitmap ResizeBitmap(Bitmap imgToResize, int newWidth)
+        {
+            Bitmap b = new Bitmap(newWidth, newWidth);
+            using (Graphics g = Graphics.FromImage(b))
+            {
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.DrawImage(imgToResize, 0, 0, newWidth, newWidth);
+            }
+            return b;
+        }
+
+        private string BitmapToSvgBase64(Bitmap bitmap)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                byte[] imageBytes = ms.ToArray();
+                string base64String = Convert.ToBase64String(imageBytes);
+                return $"<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"{bitmap.Width}\" height=\"{bitmap.Height}\"><image href=\"data:image/png;base64,{base64String}\" width=\"{bitmap.Width}\" height=\"{bitmap.Height}\" /></svg>";
+            }
+        }
+
         private void btnSaveQRSVG_Click(object sender, EventArgs e)
         {
+            // Získání nové velikosti
+            int newWidth;
+            if (!int.TryParse(widthBox.Text, out newWidth))
+            {
+                MessageBox.Show("Zadal jsi špatnou velikost");
+                return;
+            }
 
-            // Zajistěte, že máte stále přístup k původnímu qrCodeData
-            QRCodeGenerator qrGenerator = new QRCodeGenerator();
-            QRCodeData qrCodeData = qrGenerator.CreateQrCode(QRtext_SVG, QRCodeGenerator.ECCLevel.Q);
-            SvgQRCode svgQRCode = new SvgQRCode(qrCodeData);
+            // Vygenerování QR kódu jako bitmapa
+            Bitmap qrCodeBitmap = GenerateQrCodeBitmap(QRtext_SVG, newWidth);
 
-            // Získání SVG jako řetězce
-            string svgCode = svgQRCode.GetGraphic(20);
+            // Změna velikosti bitmapy
+            Bitmap resizedBitmap = ResizeBitmap(qrCodeBitmap, newWidth);
 
-            // Uložení QR kódu do SVG souboru
+            // Převedení bitmapy na SVG
+            string resizedSvgCode = BitmapToSvgBase64(resizedBitmap);
+
+            // Uložení SVG kódu do souboru
             using (SaveFileDialog saveFileDialog = new SaveFileDialog())
             {
                 saveFileDialog.Filter = "SVG Image|*.svg";
@@ -73,11 +150,61 @@ namespace Transfer_IMG.Popup
                     if (!string.IsNullOrEmpty(saveFileDialog.FileName))
                     {
                         // Uložení SVG kódu do souboru
-                        System.IO.File.WriteAllText(saveFileDialog.FileName, svgCode);
+                        System.IO.File.WriteAllText(saveFileDialog.FileName, resizedSvgCode);
                         saveLabel_dysplayed();
                     }
                 }
             }
         }
+
+
+
+        private void savAsPDF_Click(object sender, EventArgs e)
+        {
+            // Bitmapa pro zobrazení QR kódu
+            Bitmap qrCodeImage = (Bitmap)qrCodePictureBox.Image;
+
+            // Uložení QR kódu do souboru jako PDF
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "PDF Document|*.pdf";
+                saveFileDialog.Title = "Save QR Code as PDF";
+                saveFileDialog.FileName = "QRCode.pdf";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    if (!string.IsNullOrEmpty(saveFileDialog.FileName))
+                    {
+                        // Vytvoření PDF dokumentu
+                        using (PdfDocument document = new PdfDocument())
+                        {
+                            // Přidání stránky
+                            PdfPage page = document.AddPage();
+
+                            // Nastavení grafiky pro vykreslování na stránku
+                            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+                            // Vytvoření XImage z Bitmapy
+                            using (MemoryStream ms = new MemoryStream())
+                            {
+                                qrCodeImage.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                                XImage img = XImage.FromStream(ms);
+
+                                // Vykreslení obrázku na střed stránky
+                                double x = (page.Width - img.PointWidth) / 2;
+                                double y = (page.Height - img.PointHeight) / 2;
+
+                                gfx.DrawImage(img, x, y, img.PointWidth, img.PointHeight);
+                            }
+
+                            // Uložení PDF dokumentu
+                            document.Save(saveFileDialog.FileName);
+                        }
+                        saveLabel_dysplayed();
+                    }
+                }
+            }
+        }
+
     }
 }
